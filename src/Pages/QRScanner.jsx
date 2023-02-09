5b8e57e35e872a9ee74f576d465c8102adb99a5e
Victor <hi4u44r306@gmail.com>
2023-02-09 14:30:40 +0800

    .

--- src/Pages/QRScanner.jsx
+++ src/Pages/QRScanner.jsx
@@ -1,9 +1,47 @@
 // QRScanner.js
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import QrReader from 'react-weblineindia-qrcode-scanner'
 import './scss/QRScanner.scss';
+import Quagga from 'quagga';
+
 
 const QRScanner = () => {
+
+    const scannerContainer = useRef(null);
+    useEffect(() => {
+        Quagga.init({
+            inputStream: {
+                type: 'LiveStream',
+                constraints: {
+                    width: 640,
+                    height: 480,
+                    facingMode: 'environment'
+                },
+            },
+            locator: {
+                patchSize: 'medium',
+                halfSample: true
+            },
+            numOfWorkers: 2,
+            decoder: {
+                readers: ['code_128_reader']
+            },
+            locate: true
+        }, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            Quagga.start();
+        });
+
+        Quagga.onDetected(function (result) {
+            console.log(result.codeResult.code);
+        });
+
+        return () => {
+            Quagga.stop();
+        };
+    }, []);
     const [result, setResult] = useState(null);
 
     const handleScan = (data) => {
@@ -19,8 +57,8 @@ const QRScanner = () => {
     return (
         <div className="qr-reader">
             <h3>會員掃描</h3>
-            {/* <canvas> */}
-            <QrReader
+            <div ref={scannerContainer} id="scanner-container"></div>
+            {/* <QrReader
                 delay={300}
                 facingMode={"environment"}
                 style={{
@@ -29,7 +67,7 @@ const QRScanner = () => {
                 }}
                 onError={handleError}
                 onScan={handleScan}
-            />
+            /> */}
             {/* </canvas> */}
 
             {/* {result && (
